2a16eb639961818d7fda5cb77bb560b970b9db10
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2020-11-27 15:33:55 +0100

    electra

--- loss/Dist.py
+++ loss/Dist.py
@@ -9,5 +9,4 @@ class DistLoss(torch.nn.Module):
         self.cos = nn.CosineSimilarity()
 
     def forward(self, e, e_c):
-        # - (log σ(Z^T . HK)) + λE [log σ(ZN^T . HK)])
         return (1 - self.cos(e, e_c)).mean()
