9d08fdd75951a986acdf568a61dc2753e2e4dff1
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2020-11-30 21:57:49 +0100

    clean

--- criterion/Contrastive.py
+++ criterion/Contrastive.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
 import torch
 from torch import Tensor
-import torchaudio
-
-from models.Wav2Vec import Wav2vec
 
 
 class ContrastiveLoss(torch.nn.Module):
@@ -25,26 +21,3 @@ class ContrastiveLoss(torch.nn.Module):
     def forward(self, h_k: Tensor, z: Tensor, z_n: Tensor) -> Tensor:
         # - (log σ(Z^T . HK)) + λE [log σ(ZN^T . HK)])
         return - (self.log_sigmoid_probs(z, h_k) + self.log_sigmoid_probs(-z_n, h_k))
-
-
-# Test criterion
-if __name__ == '__main__':
-    criterion = ContrastiveLoss()
-    model = Wav2vec()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
-    waveform, sample_rate = torchaudio.load("../models/wav_16k_example.wav")
-
-    loss_values = []
-    model.train()
-    for i in range(2):
-        optimizer.zero_grad()
-        c, z, z_n = model(torch.unsqueeze(waveform, 1))
-        loss = criterion(c, z, z_n)
-        loss_values.append(loss.item())
-        loss.backward()
-        optimizer.step()
-        print(loss.item())
-
-        if i % 100 == 0:
-            plt.plot(loss_values)
-            plt.show()
--- models/Wav2VecSemantic.py
+++ models/Wav2VecSemantic.py
@@ -1,17 +1,7 @@
-# TODO: This model is the enhanced wav2vec semantic context electra model
-# https://github.com/google-research/electra
-# https://github.com/lucidrains/electra-pytorch/blob/master/electra_pytorch/electra_pytorch.py
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchaudio
 from torch import Tensor
-from transformers import ElectraTokenizer, ElectraModel
-
-from criterion.Contrastive import ContrastiveLoss
-from criterion.Dist import DistLoss
-from utils.plot_util import TSNE_Wav2Vec_embed_Semantic_embed
 
 
 def buffered_arange(max):
@@ -227,44 +217,5 @@ class Wav2VecPrediction(nn.Module):
         c = self.transpose_context(c.unsqueeze(-1))
         # get distractor samples
         z_n = self.sample_negatives(z)
+
         return c, z, z_n
-
-
-if __name__ == '__main__':
-    wav_model = Wav2vecSemantic()
-
-    con_criterion = ContrastiveLoss()
-    dist_criterion = DistLoss()
-
-    tokenizer = ElectraTokenizer.from_pretrained('google/electra-small-discriminator')
-    electra_model = ElectraModel.from_pretrained('google/electra-small-discriminator', return_dict=True)
-
-    text_1 = "SHE HAD THIN AWKWARD FIGURE".lower()
-    waveform, sample_rate = torchaudio.load("./wav_16k_example.wav")
-    optimizer = torch.optim.Adam(wav_model.parameters(), lr=0.02)
-
-    loss_dist_values = []
-    wav_model.train()
-
-    pos = []
-
-    for i in range(2):
-        optimizer.zero_grad()
-        tokens = torch.tensor(tokenizer.encode(text_1, return_tensors="pt"))
-        e = electra_model(tokens)[0]
-        print(e.shape)
-        print(tokens.shape)
-        (hk, z, z_n), e_c = wav_model(torch.unsqueeze(waveform, 1), tokens.shape[1])
-
-        loss_dist = dist_criterion(e, e_c)
-        loss_con = con_criterion(hk, z, z_n)
-        loss = (loss_dist + loss_con) / 2
-
-        loss_dist_values.append(loss.item())
-        loss.backward()
-
-        optimizer.step()
-        print(loss_dist.item())
-
-        TSNE_Wav2Vec_embed_Semantic_embed([e_c.view(-1).cpu().detach().numpy(), e.view(-1).cpu().detach().numpy()],
-                                          batch_n=1)
