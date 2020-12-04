daaa59e803b72d923b36a38e47626c00087819fa
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2020-12-05 00:03:53 +0100

    b16

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
     if args.loss not in ["con", "triplet", "con_triplet"]:
         exit(1)
 
-    batch_size = 32
+    batch_size = 16
     train_loader = DataLoader(dataset=train_data,
                               batch_size=batch_size,
                               pin_memory=True,
