75ece032800100c625b06fa11c79718aafe4ecdf
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2020-12-04 22:35:20 +0100

    only 16 works

--- jobscript_con_triplet.sh
+++ jobscript_con_triplet.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
  #BSUB -q gpuv100
  #BSUB -gpu "num=1"
- #BSUB -J con_triplet
+ #BSUB -J con
  #BSUB -n 1
- #BSUB -W 10:00
+ #BSUB -W 24:00
+ #BSUB -R "span[hosts=1]"
  #BSUB -R "rusage[mem=32GB]"
  #BSUB -o logs/%J.out
  #BSUB -e logs/%J.err
